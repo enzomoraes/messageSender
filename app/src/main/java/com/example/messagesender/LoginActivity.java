7d250ccfff0a2003c01fe84d3f2217fc7d1d87b0
Enzo Moraes <enzomoraes12@hotmail.com>
2020-06-25 12:37:14 -0400

    Feature de Login e Cadastro de Usuários com foto no Firebase

--- app/src/main/java/com/example/messagesender/LoginActivity.java
+++ app/src/main/java/com/example/messagesender/LoginActivity.java
@@ -7,8 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -32,8 +40,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEditEmail.getText().toString();
                 String senha = mEditSenha.getText().toString();
+
                 Log.i("Teste", email);
                 Log.i("Teste", senha);
+
+                if (email == null || email.isEmpty() || senha == null || senha.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Senha e email devem ser preenchidos!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i("Teste", task.getResult().getUser().getUid());
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("Teste", e.getMessage());
+                            }
+                        });
             }
         });
 
